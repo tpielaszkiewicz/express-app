4b68f775172faf15ed8c948ed4daf34191c18cf8
tpielaszkiewicz <30354909+tpielaszkiewicz@users.noreply.github.com>
2020-09-21 14:43:20 +0200

    port return

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/', function (req, res) {
 
 
 // Start server 
-// var port = process.env.PORT || 3000;
-var port = 3000;
+var port = process.env.PORT || 3000;
+// var port = 3000;
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}`);
 });
