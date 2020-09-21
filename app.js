72d84c70792bfa81f425a58a1093ed6b23e8f46e
tpielaszkiewicz <30354909+tpielaszkiewicz@users.noreply.github.com>
2020-09-21 14:12:19 +0200

    port return

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.get('/', function (req, res) {
 // Start server 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log(`Example app listening on port ${port}`);
 });
